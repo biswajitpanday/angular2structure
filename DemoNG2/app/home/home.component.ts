c02943fd486c35e9a45c6272085f98cdfb24d4b0
biswajit <biswajitmailid@gmail.com>
2017-02-07 21:36:33 +0600

    Angular 2 structure.

---
+++ DemoNG2/app/home/home.component.ts
@@ -0,0 +1,13 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-home',
+    template: '<h1>{{welcome}}</h1>'
+})
+
+export class HomeComponent {
+    welcome: string;
+    constructor() {
+        this.welcome = "This is home page.";
+    }
+};
